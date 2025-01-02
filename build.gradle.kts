ce2a61f224f28db45a0bd2410ca840a69442fd8b
Diego Mondego <diego@orangebox.technology>
2025-01-02 18:53:35 -0300

    ok 2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.kordamp.gradle.plugin.jandex.tasks.JandexTask
 
 plugins {
-    id("org.kordamp.gradle.jandex") version "1.1.0"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.allopen") version "1.9.22"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.allopen") version "2.0.21"
     id("io.quarkus")
     id("maven-publish")
+    id("org.kordamp.gradle.jandex") version "2.1.0"
 }
 
 repositories {
@@ -22,23 +22,23 @@ val quarkusPlatformVersion: String by project
 
 dependencies {
     implementation(platform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
-    implementation("io.quarkus:quarkus-resteasy-reactive")
+    implementation("io.quarkus:quarkus-rest-jsonb")
     implementation("io.quarkus:quarkus-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
 
-    implementation("com.orangebox.kit.core:orangekit-core:2.0.1")
+    implementation("com.orangebox.kit.core:orangekit-core:2.1.0")
 
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
 
 group = "com.orangebox.kit.authkey"
-version = "2.0.0"
+version = "2.1.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<Test> {
@@ -47,18 +47,12 @@ tasks.withType<Test> {
 
 tasks.withType<JandexTask> {
     dependsOn(":quarkusDependenciesBuild")
-    dependsOn(":test")
 }
 
 allOpen {
     annotation("javax.ws.rs.Path")
     annotation("javax.enterprise.context.ApplicationScoped")
     annotation("io.quarkus.test.junit.QuarkusTest")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
-    kotlinOptions.javaParameters = true
 }
 
 publishing {
